c6b997ed1bd4a33b2ea9cfffa8b59d22587eafd6
hamza-halawa2020 <h.halawa2020@gmail.com>
2025-05-05 22:04:55 +0300

    update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    backEndUrl: 'https://new-backend.rimansan.net/public/api',
-    imgUrl: 'https://new-backend.rimansan.net/public/images/',
-    i18nUrl: 'https://new-backend.rimansan.net/public/i18n/',
+    backEndUrl: 'https://backend.rimansan.net/public/api',
+    imgUrl: 'https://backend.rimansan.net/public/images/',
+    i18nUrl: 'https://backend.rimansan.net/public/i18n/',
 
 };
